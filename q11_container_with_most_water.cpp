1c0ba4a0488d79a37e3bcc6565ec6a2f1dac79ef
Zhishi Zeng <zudaojun@outlook.com>
2019-08-14 05:55:52 +0800

    udpate sort for q11

--- q11_container_with_most_water.cpp
+++ q11_container_with_most_water.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <cmath>
 
 using namespace std;
 
+struct index_height{
+    int index;
+    int height;
+};
+
+bool index_height_compare(struct index_height i, struct index_height j)
+{
+    return i.height > j.height;
+}
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        if (height.size() == 2) return min(height[0], height[1]);
-
+        vector<struct index_height> index_heights;
+        int i, j;
         int max_square = 0, high, square;
-        for (int i = 0; i < height.size() - 1; ++i) {
-            for (int j = i; j < height.size(); ++j) {
-                high = min(height[i], height[j]);
-                square = (j - i) * high;
-                max_square = max(max_square, square);
-            }
+
+        if (height.size() == 2) return min(height[0], height[1]);
+        for (i = 0; i < height.size(); ++i) {
+            struct index_height p = {
+                index: i,
+                height: height[i]
+            };
+            index_heights.push_back(p);
         }
+        sort(index_heights.begin(), index_heights.end(), index_height_compare);
+
+        for (i = 1; i < index_heights.size(); ++i) {
+            struct index_height ihigh = index_heights[i];
+            int max_length = 0; 
+            for (j = 0; j < i; ++j) {
+                max_length = max(max_length, abs(ihigh.index - index_heights[j].index));
+            }
+            max_square = max(max_square, max_length * ihigh.height);
+        }
+
         return max_square;
     }
 };
