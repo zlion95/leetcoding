7fedf0262d20f0de2d3691ed3ef2d0851ce4955e
Zhishi Zeng <zudaojun@outlook.com>
2019-08-14 10:53:23 +0800

    update q12 with faster match and less memory use

--- q12_int_to_roman.cpp
+++ q12_int_to_roman.cpp
@@ -34,70 +34,35 @@ public:
         }
     }
 
-    bool is3to4(char *s, int len, int seed) {
-        char s1 = get1(seed);
-
-        if (len < 3) return false;
-        if (s[len - 3] == s1 && s[len - 2] == s1 && s[len - 1] == s1)
-            return true;
-        return false;
-    }
-
-    bool is4to5(char *s, int len, int seed) {
-        char s1 = get1(seed), s5 = get5(seed);
-
-        if (len < 2) return false;
-        if (s[len - 2] == s1 && s[len - 1] == s5) return true;
-        return false;
-    }
-    
-    bool is8to9(char *s, int len, int seed) {
-        char s1 = get1(seed), s5 = get5(seed);
-
-        if (len < 4) return false;
-        if (s[len - 4] == s5
-                && s[len - 3] == s1
-                && s[len - 2] == s1
-                && s[len - 1] == s1)
-            return true;
-        return false;
-    }
-
-    string auto_reduce(int num, int seed) {
-        int len = 0;
-        char *s = (char *)malloc(sizeof(char) * 16);
+    void auto_reduce(int num, int seed, char *s) {
+        int i = 0;
         char s1 = get1(seed), s5 = get5(seed), s10 = get1(seed + 1);
-        string str;
-        memset(s, 0, sizeof(char) * 16);
 
-        for(; num > 0; --num) {
-            if (is8to9(s, len, seed)) {
-                s[len - 4] = s1;
-                s[len - 3] = s10;
-                s[len - 2] = '\0';
-            } else if (is3to4(s, len, seed)) {
-                s[len - 3] = s1;
-                s[len - 2] = s5;
-                s[len - 1] = '\0';
-            } else if (is4to5(s, len, seed)) {
-                s[len - 2] = s5;
-                s[len - 1] = '\0';
-            } else {
-                s[len] = s1;
-                s[len + 1] = '\0';
-            }
-            len = strlen(s);
+        if (num == 9) {
+            s[0] = s1;
+            s[1] = s10;
+        } else if (num >= 5) {
+            s[0] = s5;
+            i = 1;
+            while (num - 4 - i > 0) s[i++] = s1;
+        } else if (num == 4) {
+            s[0] = s1;
+            s[1] = s5;
+        } else {
+            i = 0;
+            while (num - i > 0) s[i++] = s1;
         }
-        str = s;
-        return str;
     }
 
     string intToRoman(int num) {
         int n = 0, seed = 0;
+        char cstr[5];
         string str = "";
         while (num != 0) {
             n = num % 10;
-            str = auto_reduce(n, seed) + str;
+            memset(cstr, 0, sizeof(cstr));
+            auto_reduce(n, seed, cstr);
+            str = cstr + str;
             num /= 10;
             seed += 1;
         }
