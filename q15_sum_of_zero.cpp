8f923121f570eb01e016bd0a20b85cee53d48667
Zhishi Zeng <zudaojun@outlook.com>
2019-08-15 11:52:23 +0800

    update q15 and some test_case

--- q15_sum_of_zero.cpp
+++ q15_sum_of_zero.cpp
@@ -19,19 +19,22 @@ public:
     void proper_insert(vector<int> &list, int value) {
         for (vector<int>::iterator it = list.begin();
                 it != list.end(); ++it) {
-            if (value == *it) break;
+            if (value == *it) return;
             if (value > *it) {
-                list.insert(it, value);
-                break;
+                list.insert(it + 1, value);
+                return;
             }
         }
+        list.insert(list.begin(), value);
     }
 
     vector<vector<int> > threeSum(vector<int>& nums) {
         vector<vector<int> > sum_pair;
         vector<int> sorted_keys;
         map<int, int> value_maps;
-        map<int, int>::iterator it, zs_it, ps_it;
+        map<int, int>::iterator it;
+        vector<int>::iterator zs_it, ps_it;
+        int value, minus_end, zstart, pstart;
         
         if (nums.size() < 2) return sum_pair;
 
@@ -41,24 +44,45 @@ public:
             if (it == value_maps.end()) {
                 value_maps[nums[i]] = 1;
                 proper_insert(sorted_keys, nums[i]);
+                cout << nums[i] << endl;
             }
-            else *it += 1;
+            else *it++;
         }
 
-        zs_it = sorted_keys.end();
-        ps_it = sorted_keys.begin();
-        while(ps_it != sorted_keys.end()) { 
-            if (*ps_it == 0 && zs_it == sorted_keys.end()) zs_it = ps_it;
-            else if (*ps_it > 0) break;
-            else ps_it++;
+        zstart = sorted_keys.size();
+        pstart = 0;
+        while (pstart < sorted_keys.size()) {
+            if (sorted_keys[pstart] == 0 && zstart == sorted_keys.size()) zstart = pstart;
+            else if (sorted_keys[pstart] > 0) break;
+            else pstart++;
         }
-        if (zs_it != sorted_keys.end() && ps_it - zs_it > 2)
-            add_pair(sum_pair, 0, 0, 0);
-        if (ps_it == sorted_keys.end()) return sum_pair;
-        if (ps_it == sorted_keys.begin() || zs_it == sorted_keys.begin())
-            return sum_pair;
+        cout << "zstart: " << zstart << " pstart: " << pstart << endl;
+        if (zstart < sorted_keys.size() && pstart - zstart > 2) add_pair(sum_pair, 0, 0, 0);
+        if (pstart == sorted_keys.size()) return sum_pair;
+        if (pstart == 0 || zstart == 0) return sum_pair;
         
+        minus_end = min(zstart, pstart);
+        if (minus_end >= 2) {
+            for (int i = 1; i < minus_end; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    value = -(sorted_keys[i] + sorted_keys[j]);
+                    cout << value << endl;
+                    it = value_maps.find(value);
+                    if (it == value_maps.end()) add_pair(sum_pair, sorted_keys[i], sorted_keys[j], value);
+                }
+            }
+        }
 
+        if (sorted_keys.size() - pstart >= 2) {
+            for (int i = pstart + 1; i < sorted_keys.size(); ++i) {
+                for (int j = pstart; j < i; ++j) {
+                    value = -(sorted_keys[i] + sorted_keys[j]);
+                    cout << value << endl;
+                    it = value_maps.find(value);
+                    if (it == value_maps.end()) add_pair(sum_pair, sorted_keys[i], sorted_keys[j], value);
+                }
+            }
+        }
 
         return sum_pair;
     }
