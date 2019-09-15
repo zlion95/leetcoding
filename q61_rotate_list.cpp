c52d5aad768906a274bb9607aa55872f57c9e704
zlion95 <zudaojun@outlook.com>
2019-09-16 00:13:02 +0800

    Add q61 and its case

---
+++ q61_rotate_list.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "linked_list.h"
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        int count = 0;
+        ListNode *l1 = head, *l2 = l1;
+        
+        if (!head) return head;
+        while (k && l2) {
+            l2 = l2->next;
+            --k;
+            ++count;
+        }
+        if (k == 0 && !l2) return head;
+        else if (k > 0) return rotateRight(head, k % count);
+
+        while (l2->next != NULL) {
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        
+        l2->next = head;
+        head = l1->next;
+        l1->next = NULL;
+
+        return head;
+    }
+};
+
+int main(int argc, char **argv) {
+    int n, value;
+    Solution s;
+    ListNode *head = new ListNode(-1), *p = head;
+    
+    cin >> n;
+    while (n) {
+        cin >> value;
+        p->next = new ListNode(value);
+        p = p->next;
+        n--;
+    }
+    cin >> n;
+
+    printList(head->next);
+    p = s.rotateRight(head->next, n);
+    printList(p);
+}
