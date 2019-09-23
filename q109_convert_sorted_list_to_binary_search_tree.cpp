b2a706779be3886ca3ed176405a97d537ce2ee61
zlion95 <zudaojun@outlook.com>
2019-09-23 20:13:22 +0800

    Update 109 and it's case

--- q109_convert_sorted_list_to_binary_search_tree.cpp
+++ q109_convert_sorted_list_to_binary_search_tree.cpp
@@ -4,13 +4,46 @@
 
 using namespace std;
 
+//Solution 1: recursion method
 class Solution {
 public:
-    TreeNode* sortedListToBST(ListNode* head) {
-        TreeNode *root = new TreeNode(0);
+    TreeNode* sortedListToBSTWithSize(ListNode *head, int count) {
+        TreeNode *root;
+        ListNode *p = head, *left = NULL, *right;
+        int mid = count / 2;
+
+        if (head == NULL) return NULL;
+        while (mid) {
+            left = p;
+            p = p->next;
+            --mid;
+        }
+        if (left)
+            left->next = NULL;
+        right = p->next;
+        p->next = NULL;
+        root = new TreeNode(p->val);
+        //delete p;
+
+        if (head != p) {
+            root->left = sortedListToBSTWithSize(head, count / 2);
+            root->right = sortedListToBSTWithSize(right, (count - 1) / 2);
+        }
         return root;
     }
+
+    TreeNode* sortedListToBST(ListNode* head) {
+        ListNode *p = head;
+        int count = 0;
+
+        while (p) {
+            ++count;
+            p = p->next;
+        }
+        return sortedListToBSTWithSize(head, count);
+    }
 };
+
 
 int main(int argc, char **argv) {
     int n, value;
